2c7240f17ec44d60e30bf476888485c8fab1edec
MinChanJu <mcj00220077@gmail.com>
2024-12-31 13:06:14 +0900

    재 업로드

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // React Router 추가
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* React Router로 App 컴포넌트 감싸기 */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
